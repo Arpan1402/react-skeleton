a290cf395b9d8e889d701d2c6d7bf451b3441b9f
Arpan <arpandas1402@gmail.com>
2024-02-20 03:13:18 +0530

    Skeleton created

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Icon from "./assests/icon.png"
+
+const App = () => {
+    return(
+        <div>
+            <img src={Icon} alt="" />
+            <h1>This is basic react skeleton</h1>
+        </div>
+    )
+}
+
+export default App
